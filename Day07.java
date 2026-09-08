c8ff5700c2cfba72e9211b88c50614a2f368646a
Ahmad Syarif Putra <ahmadsyarifputra2008@gmail.com>
2026-09-08 21:45:24 +0800

    Add Day07.java with String data type example
    Demonstrate usage of String data type in Java.

---
+++ Day07.java
@@ -0,0 +1,18 @@
+public class Day07 {
+    public static void main(String[] args) {
+        // PENGENALAN TIPE  DATA STRING UNTUK TEKS
+        /*
+         String -> tipe data ini untuk menyimpan teks atau kumpulan karakter.
+         String dapat menyimpan huruf, angka, simbol, dan spasi.
+        */
+
+        //contoh penggunaan tipe data String
+        System.out.println("\nContoh penggunaan tipe data String:");
+        String namaDepan = "John";
+        String namaBelakang = "Doe";
+        System.out.println("Nama Depan: " + namaDepan);
+        System.out.println("Nama Belakang: " + namaBelakang);
+        System.out.println("Nama Lengkap: " + namaDepan + " " + namaBelakang);
+        
+    }
+}
