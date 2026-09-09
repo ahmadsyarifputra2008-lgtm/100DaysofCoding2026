a874a1ffb81ae6c5045ac3985292ab8f4285fac3
Ahmad Syarif Putra <ahmadsyarifputra2008@gmail.com>
2026-09-09 16:10:11 +0800

    Add Day09 class demonstrating constant usage

---
+++ Day09.java
@@ -0,0 +1,20 @@
+public class Day09 {
+    public static void main(String[] args) {
+        // KONSTANTA (final variabel)
+
+        /*
+         Konstanta adalah variabel yang nilainya tidak dapat diubah setelah diinisialisasi.
+         Untuk mendeklarasikan konstanta, kita menggunakan kata kunci final.
+        */
+
+        //contoh penggunaan konstanta
+        System.out.println("\nContoh penggunaan konstanta:");
+        final double PI = 3.14159; // deklarasi konstanta PI dengan tipe data double
+        System.out.println("nilai PI ="+PI+"\n");
+
+        final byte HARI_SEMINGGU = 7;
+        System.out.println("hari dalam seminggu = "+ HARI_SEMINGGU + " HARI");
+        
+        
+    }
+}
