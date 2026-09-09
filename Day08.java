e19cc92e1e09acdf8a6aa03b92e00d4b60685fed
Ahmad Syarif Putra <ahmadsyarifputra2008@gmail.com>
2026-09-09 16:00:28 +0800

    Add Day08 class with variable declaration example

---
+++ Day08.java
@@ -0,0 +1,13 @@
+public class Day08 {
+    public static void main(String[] args) {
+       // DEKLARASI DAN INISIALISASI DAN UPDATE 
+       
+       //Contoh deklarasi dan inisialisasi variabel
+         System.out.println("\nContoh deklarasi dan inisialisasi variabel:");
+        int umur = 25; // deklarasi variabel umur dengan tipe data int dan inisialisasi dengan nilai 25
+        System.out.println("Umur: " + umur);
+        umur = 30; // update nilai variabel umur
+        System.out.println("Umur setelah diupdate: " + umur);
+        
+    }
+}
