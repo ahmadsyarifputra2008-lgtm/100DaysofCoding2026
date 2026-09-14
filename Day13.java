a92e89b24b137229a875164a8a10ad314ae00d66
Ahmad Syarif Putra <ahmadsyarifputra2008@gmail.com>
2026-09-14 17:43:59 +0800

    Add Evaluasi class for academic profile display

---
+++ Day13.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class Evaluasi{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        String a, b, c, d, e;
+        
+
+        a = input.nextLine();
+        b = input.nextLine();
+        c = input.nextLine();
+        d = input.nextLine();
+        e = input.nextLine();
+
+        System.out.println("===== PROFIL AKADEMIK =====");
+        System.out.println("nama \t\t\t:"+a);
+        System.out.println("NIM \t\t\t:"+b);
+        System.out.println("Program Studi \t\t:"+c);
+        System.out.println("fakultas \t\t:"+d);
+        System.out.println("perguruan Tinggi \t:"+e);
+        System.out.println("===============================");
+    
+
+
+
+
+
+    }
+}
+    
