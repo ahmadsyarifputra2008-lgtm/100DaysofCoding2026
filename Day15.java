94f8a7339e52008cba1e04fc88acfa273cb53c5a
Ahmad Syarif Putra <ahmadsyarifputra2008@gmail.com>
2026-09-16 22:57:56 +0800

    Refactor multiplication and add division operation
    Updated output messages and added division functionality.

--- Day15.java
+++ Day15.java
@@ -4,8 +4,8 @@ public class Day15 {
     public static void main(String[] args) {
         try ( // Operasi aritmatika perkalian(*) dan pembagian(/)
                 Scanner input = new Scanner(System.in)) {
-            System.out.println("Masukkan Nilai nya ngab!!!");
-            
+            System.out.println("PERKALIAN");
+            System.out.println("Masukkan Nilai nya ngab!!!");  
             System.out.print("Nilai pertama : ");
             int x = input.nextInt();
             
@@ -13,10 +13,18 @@ public class Day15 {
             int y = input.nextInt();
             
             int z = x * y;
-            double m = x / y;
-            System.out.println("hasil perkalian : "+z);
+            System.out.println("hasil perkalian : "+z+"\n");
+
+            System.out.println("PEMBAGIAN");
+
+            System.out.print("Nilai pertama : ");
+            double a = input.nextDouble();
+            
+            System.out.print("Nilai kedua : ");
+            double b = input.nextDouble();
+
+            double m = a / b;
             System.out.println("hasil pembagian: "+m);
-            input.close();
         }
 
     }
