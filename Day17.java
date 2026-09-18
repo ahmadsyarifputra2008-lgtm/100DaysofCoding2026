2856b7cd15453aa14c7cd02d6c7fbca2596721f9
Ahmad Syarif Putra <ahmadsyarifputra2008@gmail.com>
2026-09-18 20:02:02 +0800

    Refactor Day17.java to use Scanner for input

--- Day17.java
+++ Day17.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 public class Day17 {
     public static void main(String[] args) {
         // OPERASI PENUGASAN (==, +=, -=, <=, >=, DLL)
@@ -11,30 +14,34 @@ public class Day17 {
         *= : Menyederhanakan operasi x = x * y.
         */
 
+        Scanner in = new Scanner (System.in);
+
+
         System.out.println("== OPERASI PENUGASAN ==");
         // Okee kita kasi nilai misalnya x = 10, dan y = 5
-
-        int x = 10; //disini kita tidak perlu menyatakan variabel y, karna kita langsung saja tambahkan ke dalam operasi nya
+        System.out.print("masukkan nilai X : ");
+        int x = in.nextInt(); //disini kita tidak perlu menyatakan variabel y, karna kita langsung saja tambahkan ke dalam operasi nya
          
 
-        // +=
+        // += -> penugasan penjumlahan
         int a = x += 5;
         System.out.println("+= : x + y = "+a);
 
-        // -=
+        // -= -> penugasan Pengurangan
         int b = x -= 5;
         System.out.println("-+ : x - y = "+b);
 
-        // *=
+        // *= -> penugasan Perkalian
         int c = x *= 5;
         System.out.println("*= : x * y = "+c);
 
-        // /=
+        // /= -> penugasan Pembagian
         int d = x /= 5;
         System.out.println("/= : x / y = "+d);
 
-        // %=
+        // %= -> penugasan Sisa Bagi
         int e = x %= 5;
         System.out.println("%= : x % y = "+e);
+        in.close();
     }
 }
